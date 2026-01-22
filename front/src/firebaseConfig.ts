d4b46b758926841093d04163e54e146cc7eeb0c6
moudjane <mathis.oudjane@gmail.com>
2026-01-22 10:29:23 +0100

    update firebaseConfig

--- front/src/firebaseConfig.ts
+++ front/src/firebaseConfig.ts
@@ -3,11 +3,11 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  authDomain: "project-web-ai-3d052.firebaseapp.com",
+  projectId: "project-web-ai-3d052",
+  storageBucket: "project-web-ai-3d052.firebasestorage.app",
+  messagingSenderId: "245388012694",
+  appId: "1:245388012694:web:2993b2db20bab1070b26b7"
 };
 
 const app = initializeApp(firebaseConfig);
